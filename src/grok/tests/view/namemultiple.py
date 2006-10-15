c27c85e8571d2e60f4addea6d12b3e3dce7f980c
Philipp von Weitershausen <philipp@weitershausen.de>
2006-10-15 13:12:28 +0000

    test that you can't call grok.name more than once on a class

---
+++ src/grok/tests/view/namemultiple.py
@@ -0,0 +1,9 @@
+"""
+You can't call grok.name multiple times for a view
+
+  >>> import grok.tests.view.namemultiple_fixture
+  Traceback (most recent call last):
+    ...
+  GrokError: grok.name can only be called once per class.
+
+"""
---
+++ src/grok/tests/view/namemultiple_fixture.py
@@ -0,0 +1,5 @@
+import grok
+
+class MultipleNames(grok.View):
+    grok.name('mammoth')
+    grok.name('bear')
