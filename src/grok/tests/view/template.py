035dcb6d1d425cb0268b9e2e022828e35de03832
Philipp von Weitershausen <philipp@weitershausen.de>
2006-10-16 07:20:58 +0000

    test that grok.name and grok.template can be combined

--- src/grok/tests/view/template.py
+++ src/grok/tests/view/template.py
@@ -13,6 +13,12 @@ View with an associated PageTemplate that is referred to using
   >>> print view()
   <html><body><h1>GROK PAINT MAMMOTH!</h1></body></html>
 
+``grok.name`` and ``grok.template`` can be combined:
+
+  >>> view = component.getMultiAdapter((manfred, request), name='meal')
+  >>> print view()
+  <html><body><h1>GROK EAT MAMMOTH!</h1></body></html>
+
 """
 import grok
 
@@ -24,4 +30,12 @@ class Painting(grok.View):
 
 cavepainting = grok.PageTemplate("""\
 <html><body><h1>GROK PAINT MAMMOTH!</h1></body></html>
+""")
+
+class Food(grok.View):
+    grok.template('food_template')
+    grok.name('meal')
+
+food_template = grok.PageTemplate("""\
+<html><body><h1>GROK EAT MAMMOTH!</h1></body></html>
 """)
