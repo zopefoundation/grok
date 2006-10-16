ee0aa52d3274653cc6e1476f7e837c0dc47061bf
Christian Theune <ct@gocept.com>
2006-10-16 13:16:56 +0000

    - triple XXX on super()

--- doc/design/annotations.py
+++ doc/design/annotations.py
@@ -7,7 +7,8 @@ class Comments(grok.Annotation):
     grok.context(Article)  # this is actually the default
     grok.implements(IComments)
 
-    def __init__(self):
+    def __init__(self): 
+        # XXX need super?!?
         self.comments = OOTreeSet()
 
     def addComment(self, text):
