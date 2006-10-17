c83c5c66235c43776fb667ba8b4e725375a6805f
Wolfgang Schnerring <wosc@wosc.de>
2006-10-17 13:32:18 +0000

    added test for concrete event class

--- src/grok/tests/event/subscriber.py
+++ src/grok/tests/event/subscriber.py
@@ -6,6 +6,8 @@ You can subscribe to events using the @grok.subscribe decorator:
   >>> grok.notify(grok.ObjectCreatedEvent(manfred))
   >>> mammoths
   ['Manfred']
+  >>> mammoths2
+  ['Manfred']
 
 """
 import grok
@@ -15,7 +17,12 @@ class Mammoth(object):
         self.name = name
 
 mammoths = []
+mammoths2 = []
 
 @grok.subscribe(Mammoth, grok.IObjectCreatedEvent)
 def mammothAdded(mammoth, event):
     mammoths.append(mammoth.name)
+
+@grok.subscribe(Mammoth, grok.ObjectCreatedEvent)
+def mammothAddedInstance(mammoth, event):
+    mammoths2.append(mammoth.name)
