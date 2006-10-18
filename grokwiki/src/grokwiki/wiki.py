71c31bf840bb4ee8415df0307d727a64c4041d97
Christian Theune <ct@gocept.com>
2006-10-18 07:41:17 +0000

    - beautified template  - added css  - added list of pages

--- grokwiki/src/grokwiki/wiki.py
+++ grokwiki/src/grokwiki/wiki.py
@@ -33,28 +33,45 @@ class Index(grok.View):
 
     def before(self):
         text = self.request.form.get('wikidata')
-        wiki = self.context.__parent__
+        self.wiki = self.context.__parent__
         if text:
             links = find_wiki_links(text)
             for link in links:
-                if link not in wiki:
-                    wiki[link] = WikiPage()
+                if link not in self.wiki:
+                    self.wiki[link] = WikiPage()
             self.context.text = text
 
-        wiki_url = zapi.absoluteURL(wiki, self.request)
+        wiki_url = zapi.absoluteURL(self.wiki, self.request)
 
         self.rendered_text, replacements = LINK_PATTERN.subn(r'<a href="%s/\1">\1</a>' % wiki_url, self.context.text)
 
 
 index = grok.PageTemplate("""\
 <html>
+<head>
+    <link rel="stylesheet" tal:attributes="href static/wiki.css" type="text/css">
+</head>
 <body>
-<div tal:content="structure view/rendered_text">
+<h1 tal:content="context/__name__">WikiPage</h1>
+
+<div tal:content="structure view/rendered_text" class="wikicontent">
 </div>
+
 <hr/>
+
 <form tal:attributes="action request/URL" method="POST">
-<textarea name="wikidata" tal:content="context/text"/><br/>
-<input type="submit"/>
+<textarea name="wikidata" tal:content="context/text" cols="80" rows="20"/><br/>
+<input type="submit" value="Update"/>
 </form>
+
+<hr/>
+<h3>Other pages</h3>
+<p>
+    <span tal:repeat="page view/wiki">
+        <a tal:attributes="href string:${view/wiki/@@absolute_url}/$page"
+           tal:content="page"
+           />
+    </span>
+</p>
 </body>
 </html>""")
