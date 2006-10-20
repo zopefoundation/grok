3f3df15e6f4959a4a8a930516cf31647127c9fe5
Martijn Faassen <faassen@startifact.com>
2006-10-20 10:57:20 +0000

    Add some design notes.

--- doc/design/traversal.py
+++ doc/design/traversal.py
@@ -5,18 +5,29 @@ class Appointment(grok.Model):
 
 class Day(grok.Model):
 
-    @grok.traverse
     def getAppointment(self, number):
         if number in self.appointments:
             return Appointment(number)
         return None # try to look up views then
 
+    def traverse(self, name):
+        return self.getAppointment(int(number))
+    
 class Calendar(grok.Model):
-
-    @grok.traverse
     def getYear(self, year):
         return Year(year)
 
+    def traverse(self, name):
+        return self.getYear(int(number))
+
+# interpretation of traverse:
+
+# * do the traverse traversal first
+
+# * if this raises an error, propagate exception, do not swallow it (test)
+
+# * if this returns None, fall back on "normal" traversal for the
+    object (i.e. container traversal)
 
 """
 http://.../calendar/2006/10/13/1/
