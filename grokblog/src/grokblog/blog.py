bd6b23a4fa8cde647d56c48398f02442b6457f45
Martijn Faassen <faassen@startifact.com>
2006-12-07 21:53:27 +0000

    Add blog title and tagline, accessible through /edit.

--- grokblog/src/grokblog/blog.py
+++ grokblog/src/grokblog/blog.py
@@ -1,8 +1,16 @@
 import random
 from datetime import datetime, timedelta
+
+from zope import schema
+
 import grok
 
 class Blog(grok.Container, grok.Site):
+
+    class fields:
+        title = schema.TextLine(title=u'Title', default=u'')
+        tagline = schema.TextLine(title=u'Tagline', default=u'')
+
     def __init__(self):
         super(Blog, self).__init__()
         self['entries'] = Entries()
@@ -20,6 +28,15 @@ class BlogIndex(grok.View):
     def renderEntry(self, entry):
         return renderRest(entry.body)
 
+class BlogEdit(grok.EditForm):
+    grok.context(Blog)
+    grok.name('edit')
+
+    @grok.action('Save changes')
+    def edit(self, **data):
+        self.applyChanges(**data)
+        self.redirect(self.url(self.context))
+
 class EntriesIndex(grok.View):
     grok.context(Entries)
     grok.name('index')
@@ -32,4 +49,3 @@ def lastEntries(amount):
     return sorted(
         entries, key=lambda entry: entry.published, reverse=True
         )[:amount]
-
