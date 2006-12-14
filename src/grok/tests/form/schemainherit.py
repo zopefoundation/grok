27b169b6a471d478f434d0d6e08c76af9b6098bf
Martijn Faassen <faassen@startifact.com>
2006-12-14 21:06:07 +0000

    Add a (passing) test for schema inheritance.

---
+++ src/grok/tests/form/schemainherit.py
@@ -0,0 +1,30 @@
+"""
+A grok.Model may implement a schema that inherits from another one:
+
+  >>> grok.grok(__name__)
+  >>> manfred = Mammoth()
+
+  >>> from zope import component
+  >>> from zope.publisher.browser import TestRequest
+  >>> request = TestRequest()
+  >>> view = component.getMultiAdapter((manfred, request), name='edit')
+  >>> len(view.form.form_fields)
+  3
+  >>> [w.__name__ for w in view.form.form_fields]
+  ['name', 'size', 'speciality']
+"""
+import grok
+from zope import interface, schema
+
+class IMammoth(interface.Interface):
+    name = schema.TextLine(title=u"Name")
+    size = schema.TextLine(title=u"Size", default=u"Quite normal")
+
+class ISpecialMammoth(IMammoth):
+    speciality = schema.TextLine(title=u"Speciality")
+
+class Mammoth(grok.Model):
+    interface.implements(ISpecialMammoth)
+
+class Edit(grok.EditForm):
+    grok.context(Mammoth)
