9f4b149c162eee31926abcf03de8660f76451ed5
Philipp von Weitershausen <philipp@weitershausen.de>
2007-01-09 14:18:50 +0000

    stream line for cheeseshop registration

--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='grok',
-    version='0.1',
+    version='0.9dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://svn.zope.org/grok/trunk',
-    description="""\
-Grok: Now even cavemen can use Zope 3!
-""",
+    description='Grok: Now even cavemen can use Zope 3!',
+    long_description=open('README.txt').read(),
     packages=find_packages('src'),
     package_dir = {'': 'src'},
     include_package_data = True,
     zip_safe=False,
     license='ZPL',
     
-    install_requires=['setuptools',
-                     ],
+    install_requires=['setuptools'],
 )
