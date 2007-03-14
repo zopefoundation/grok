a73d60a5b1cbb6423264587a2eaf1e83e1e1a618
Philipp von Weitershausen <philipp@weitershausen.de>
2007-03-14 13:22:44 +0000

    Do "Continuous Releases" so that you can update a grokproject that was installed automatically from svn.
    See http://peak.telecommunity.com/DevCenter/setuptools#managing-continuous-releases-using-subversion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grok',
-    version='0.9dev',
+    version='0.9',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='https://launchpad.net/grok',
