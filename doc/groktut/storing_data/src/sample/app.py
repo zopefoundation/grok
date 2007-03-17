12281d66903879a960955acbb6475e0a73a17b42
Martijn Faassen <faassen@startifact.com>
2007-03-17 15:16:09 +0000

    Sample-ize 'storing data' section.

---
+++ doc/groktut/storing_data/src/sample/app.py
@@ -0,0 +1,13 @@
+import grok
+
+class Sample(grok.Application, grok.Container):
+    text = 'default text'
+
+class Index(grok.View):
+    pass
+
+class Edit(grok.View):
+    def update(self, text=None):
+        if text is None:
+            return
+        self.context.text = text
