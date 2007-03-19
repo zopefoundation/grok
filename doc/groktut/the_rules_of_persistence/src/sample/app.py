0a458221f1cf2b448bbcaa215c89a04830a2d5f9
Martijn Faassen <faassen@startifact.com>
2007-03-19 22:54:39 +0000

    Explicitly mark our bug.

--- doc/groktut/the_rules_of_persistence/src/sample/app.py
+++ doc/groktut/the_rules_of_persistence/src/sample/app.py
@@ -12,5 +12,6 @@ class Edit(grok.View):
     def update(self, text=None):
         if text is None:
             return
+        # this code has a BUG!
         self.context.list.append(text)
         self.redirect(self.url('index'))
