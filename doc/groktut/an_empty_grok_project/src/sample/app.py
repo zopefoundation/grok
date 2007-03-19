89283c8d70626b6f247eb4768082792a0159dfaa
Martijn Faassen <faassen@startifact.com>
2007-03-19 21:56:06 +0000

    Adjust tutorial to changes in grokproject.

--- doc/groktut/an_empty_grok_project/src/sample/app.py
+++ doc/groktut/an_empty_grok_project/src/sample/app.py
@@ -1,4 +1,7 @@
 import grok
-  
-class Sample(grok.Application, grok.Container):
+
+class testproj(grok.Application, grok.Container):
     pass
+
+class Index(grok.View):
+    pass # see app_templates/index.pt
