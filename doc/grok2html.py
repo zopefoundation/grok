b21cbeff15c43b43721f56a4e7a84b7e51364a8b
Darryl Cousins <darryl@darrylcousins.net.nz>
2007-07-07 22:19:52 +0000

    Brought in macros howto from Ulifs sandbox, added link to main menu and brief index page for howto directory

--- doc/grok2html.py
+++ doc/grok2html.py
@@ -1,34 +1,18 @@
 import os
+import codecs
+import sys
 
-from zope.pagetemplate.pagetemplate import PageTemplate
+sys.path.append('/opt/odd-bobs/Zope3/src')
 
 import docutils.core
-from docutils.writers.html4css1 import HTMLTranslator
 from docutils.writers.html4css1 import Writer
 
-class ZopeTranslator(HTMLTranslator):
-    """
-    The ZopeTranslator extends the base HTML processor for reST.  It
-    augments reST by:
-
-    - Outputs *only* the 'body' parts of the document tree, using the
-    internal docutils structure.
-    """
-
-    def astext(self):
-        """
-        This is where we join the document parts that we want in
-        the output.
-        """
-        # use the title, subtitle, author, date, etc., plus the content
-        body = self.body_pre_docinfo + self.docinfo + self.body
-        return u"".join(body)
-
+from zope.app.renderer.rest import ZopeTranslator
+from zope.pagetemplate.pagetemplate import PageTemplate
+from zope.pagetemplate.pagetemplatefile import PageTemplateFile
 
 class ReStructuredTextToHTMLRenderer:
-    """convert from Restructured Text to HTML.
-
-    """
+    """Convert from Restructured Text to HTML."""
 
     def __init__(self,content):
         self.content = content 
@@ -36,94 +20,125 @@ class ReStructuredTextToHTMLRenderer:
     def render(self):
         settings_overrides = {
             'halt_level': 6,
+            'input_encoding': 'utf8',
             'output_encoding': 'utf8',
             'initial_header_level': 2
         }
-            # 'input_encoding': 'unicode',
-            # 'output_encoding': 'unicode',
+            # 'output_encoding': 'utf8',
         writer = Writer()
         writer.translator_class = ZopeTranslator
         html = docutils.core.publish_string(
-        self.content,
-        writer=writer,settings_overrides=settings_overrides,)
+                        self.content,
+                        writer=writer,
+                        settings_overrides=settings_overrides,)
         return html
 
+class RestFile(object):
+    
+    source = ''
+    target = ''
+    url = ''
+    title = ''
+    active = True
 
-Menu = [
-        {'href':'index.html','title':u'Home','klass':''},
-        {'href':'about.html','title':u'About','klass':''},
-        {'href':'tutorial.html','title':u'Tutorial','klass':''},
-        ]
+    def __init__(self, url, source, target):
+        self.url = url
+        self.target = target
+        if os.path.isfile(target):
+            if os.path.getmtime(source) < os.path.getmtime(target):
+                self.active = False
+        if os.path.isfile(source):
+            self.source = codecs.open(source,"r",'utf8').read()
+        else:
+            self.source = source
+        srctxt = self.source.split('\n')
+        title = srctxt[0]
+        if title.startswith("====="): title = srctxt[1]
+        self.title = title
+
+    def set_rest_content(self):
+        renderer = ReStructuredTextToHTMLRenderer(self.source)
+        self.content = renderer.render().strip()
+
+    def create_html(self, page, settings):
+        if not self.active:
+            pass
+        print 'Processing ', self.url
+        self.set_rest_content()
+
+        settings["context"] = Context(self.url, self.title, self.content)
+        content = page.pt_render(namespace=settings)
+        #fp = open(self.target, 'w')
+        fp = codecs.open(self.target,"w",'utf8')
+        fp.write(content)
+        fp.close()
 
 class Context:
     """Set up the context for rendering the rest as html through zpt"""
 
-    id = u''
-    title = u''
-    menu = []
-    content = u''
-
-    def __init__(self, id, title=u''):
-        self.id = id
+    def __init__(self, url, title=u'', content=u''):
+        self.url = url
         self.title = title
-
-    def restSource(self, source_file):
-        fp = open(source_file)
-        # try to locate a title for the document if not given
-        if not self.title:
-            self.title = fp.readline()
-            if self.title.startswith("="):
-                self.title = fp.readline()
-        fp.close()
-        #rest = codecs.open(source_file,"r",'utf8').read()
-        rest = open(source_file).read()
-        renderer = ReStructuredTextToHTMLRenderer(rest)
-        return renderer.render().strip()
-    
-    def setRestContent(self, source_file):
-        self.content = self.restSource(source_file)
+        self.content = content
 
     @property
     def menu(self):
         for item in Menu:
-            if item.get('href').split('.')[0] == self.id:
+            if item.get('href') == self.url:
                 item['klass'] = u'selected'
             else:
                 item['klass'] = u''
+            # just for tutorial files
+            if len(self.url.split('/')) > 2:
+                if item.get('href').split('/')[:-1] == self.url.split('/')[:-1]:
+                    item['klass'] = u'selected'
             if not item.get('description', None):
                 item['description'] = item['title']
         return Menu
 
-def handler_html(pageid, restpath):
+def create_html(rest_files, template):
+    settings = {}
+    settings["here"] = { "layout": template }
+    page = PageTemplate()
+    page.write("""<metal:block use-macro="here/layout/macros/pagelayout" />""")
 
-# we could rename about_grok.txt to about.txt that we could use only 
-# a pageid to identify the source document and the html result.
-    if os.path.exists(restpath):
-        layout_file = os.path.join('template.pt')
-        layout = PageTemplate()
-        layout.write(open(layout_file,"r").read())
+    for restfile in rest_files:
+        restfile.create_html(page, settings)
 
-        page = PageTemplate()
-        page.write("""<metal:block use-macro="here/layout/macros/pagelayout" />""")
-
-        context = Context(pageid)
-        context.setRestContent(restpath)
-
-        settings = {}
-        settings["here"] = { "layout": layout }
-        settings["context"] = context
-
-        content = page.pt_render(namespace=settings)
-
-        print content
-
+# Menu should later be generated if site becomes more complex
+Menu = [
+        {'href':'/index.html','title':u'Home','klass':''},
+        {'href':'/about.html','title':u'About','klass':''},
+        {'href':'/tutorial.html','title':u'Tutorial','klass':''},
+        {'href':'/minitutorials/index.html','title':u'How Tos','klass':''},
+        ]
 
 if __name__ == '__main__':
-    import sys
-    args = sys.argv
+    source_dir = '.'
+    www_dir = '/opt/odd-bobs/grok-www'
+    rest_files = []
+    if www_dir == '':
+        print 'Please define the target directory for html files'
+        import sys
+        sys.exit()
+    rest_files.append(RestFile('index', 
+                              os.path.join(source_dir, 'index.txt'),
+                              os.path.join(www_dir, 'index.html')))
+    rest_files.append(RestFile('about', 
+                              os.path.join(source_dir, 'about.txt'),
+                              os.path.join(www_dir, 'about.html')))
+    rest_files.append(RestFile('tutorial', 
+                              os.path.join(source_dir, 'tutorial.txt'),
+                              os.path.join(www_dir, 'tutorial.html')))
+    rest_files.append(RestFile('mini-index', 
+                              os.path.join(source_dir, 'minitutorials', 'index.txt'),
+                              os.path.join(www_dir, 'minitutorials', 'index.html')))
+    rest_files.append(RestFile('searching', 
+                              os.path.join(source_dir, 'minitutorials', 'searching.txt'),
+                              os.path.join(www_dir, 'minitutorials', 'searching.html')))
+    rest_files.append(RestFile('macros', 
+                              os.path.join(source_dir, 'minitutorials', 'macros.txt'),
+                              os.path.join(www_dir, 'minitutorials', 'macros.html')))
+    template = PageTemplateFile(os.path.join(source_dir, 'template.pt'))
+    create_html(rest_files, template)
 
-    if len(args) < 2:
-      print """  Usage: ./grok2html.py pageid restsource > pageid.html
-      Prints to stdout the results of parsing restsource with grok template."""
-    else:
-        handler_html(args[1], args[2])
