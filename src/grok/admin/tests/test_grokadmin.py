1b5863896411594f5937c22a5548f332092372be
Uli Fouquet <uli@gnufix.de>
2007-08-18 16:20:40 +0000

    Added missing license statements.

--- src/grok/admin/tests/test_grokadmin.py
+++ src/grok/admin/tests/test_grokadmin.py
@@ -1,3 +1,18 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for tests."""
+
 import unittest
 from pkg_resources import resource_listdir
 from zope.testing import doctest, cleanup
