db6a20a23654c3732d961112a798408ebdeadad5
Martijn Faassen <faassen@startifact.com>
2007-08-18 18:09:04 +0000

    Hardcode some version dependencies in an attempt to make grok work on Windows. In some cases this means we have older versions than the latest release in our dependencies, because we only have a windows binary egg for those.

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+# some of the dependencies containing C code have been hardcoded to
+# make sure we only depend on versions for which there is a windows
+# binary. In some cases this means we rely on an earlier version than the
+# latest/greatest version as no Windows binary has been released for it yet.
+
 setup(
     name='grok',
     version='0.9',
@@ -19,7 +24,7 @@ setup(
                       'martian',
                       'simplejson',
                       'pytz',
-                      'ZODB3',
+                      'ZODB3 == 3.8.0b2',
                       'zope.annotation',
                       'zope.app.apidoc',
                       'zope.app.applicationcontrol',
@@ -27,9 +32,11 @@ setup(
                       'zope.app.authentication',
                       'zope.app.catalog',
                       'zope.app.component',
-                      'zope.app.container',
+                      'zope.app.container == 3.5.0.a1',
                       'zope.app.folder',
                       'zope.app.intid',
+                      # XXX not binary, but needed for ZODB 3.8.0b22
+                      'zope.app.keyreference == 3.4.0a1',
                       'zope.app.pagetemplate',
                       'zope.app.publication',
                       'zope.app.publisher',
@@ -45,13 +52,17 @@ setup(
                       'zope.deprecation',
                       'zope.event',
                       'zope.formlib',
-                      'zope.interface',
+                      # XXX there is a later 3.4.0 release
+                      'zope.hookable == 3.4.0a1',
+                      # XXX there is a later 3.4.0 release
+                      'zope.i18nmessageid == 3.4.0a1',
+                      'zope.interface == 3.4.0',
                       'zope.lifecycleevent',
                       'zope.pagetemplate',
-                      'zope.proxy',
+                      'zope.proxy == 3.4.0',
                       'zope.publisher',
                       'zope.schema',
-                      'zope.security',
+                      'zope.security == 3.4.0b5',
                       'zope.testing',
                       'zope.traversing',
                       'zope.testbrowser',
