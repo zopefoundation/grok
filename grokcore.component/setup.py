b518d4bef5465672445210dbe71fd2c044119b36
Philipp von Weitershausen <philipp@weitershausen.de>
2007-08-22 16:02:12 +0000

    Initial package structure for grokcore.component (for now, it lives below the grok buildout of this branch).

---
+++ grokcore.component/setup.py
@@ -0,0 +1,35 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for grokcore.component package
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='grokcore.component',
+    version='0.1',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://grok.zope.org',
+    download_url='http://cheeseshop.python.org/pypi/grokcore.component/',
+    description='Support for grokking Zope components (adapters, utilities, '
+                'subscribers)',
+    long_description=open('README.txt').read(),
+    license='ZPL',
+    packages=find_packages(),
+    include_package_data = True,
+    zip_safe=False,
+    install_requires=['setuptools',
+                      'martian',
+                      ],
+)
