506c6f9437e49c09be7d56a6414cc74f572017bf
Philipp von Weitershausen <philipp@weitershausen.de>
2007-08-24 12:46:10 +0000

    The admin UI's sample application is now at the top of the list.

--- src/grok/ftests/admin/apps.py
+++ src/grok/ftests/admin/apps.py
@@ -38,8 +38,8 @@ installed yet:
 
 We are able to add a mammoth manager...
 
-  >>> browser.getControl('Name your new app:',index=13).value = 'my-mammoth-manager'
-  >>> browser.getControl('Create',index=13).click()
+  >>> browser.getControl('Name your new app:', index=1).value = 'my-mammoth-manager'
+  >>> browser.getControl('Create', index=1).click()
 
   >>> print browser.contents
   <html xmlns="http://www.w3.org/1999/xhtml">
