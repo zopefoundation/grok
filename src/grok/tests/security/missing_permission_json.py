9a7031e487fdf24035d121697c7b6659f2dfb6c3
Philipp von Weitershausen <philipp@weitershausen.de>
2007-10-10 07:53:31 +0000

    GrokErrors from the JSON grokker are now wrapped in configurationexecutionerrors, thanks to actions

--- src/grok/tests/security/missing_permission_json.py
+++ src/grok/tests/security/missing_permission_json.py
@@ -4,9 +4,8 @@ before it can be used in grok.require() in an JSON class.
 
   >>> grok.tests.grok(__name__)
   Traceback (most recent call last):
-  GrokError: Undefined permission 'doesnt.exist' in <class
-  'grok.tests.security.missing_permission_json.MissingPermission'>. Use
-  grok.Permission first.
+  ConfigurationExecutionError: martian.error.GrokError: Undefined permission 'doesnt.exist' in <class 'grok.tests.security.missing_permission_json.MissingPermission'>. Use grok.Permission first.
+  ...
 
 """
 
