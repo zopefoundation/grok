26dcf769ec0941b77bc4ccc5fe6bb22a38507b2e
Brandon Rhodes <brandon@rhodesmill.org>
2007-10-28 01:20:08 +0000

    Corrected one or two typos in the "template-languages" minitutorial, and added two basic functional tests that verify that the approach of the minitutorial works.

---
+++ src/grok/ftests/template/language_file.py
@@ -0,0 +1,71 @@
+"""
+Containers can determine how they want to be traversed by
+implementing a 'traverse' method, but the behavior falls back to
+basic container traversal if the 'traverse' method returns None:
+
+  >>> getRootFolder()["bear"] = Bear('Bjorn')
+
+Let's first try to look up the special traversed item:
+
+  >>> from zope.testbrowser.testing import Browser
+  >>> browser = Browser()
+  >>> browser.handleErrors = False
+  >>> browser.open("http://localhost/bear")
+  >>> print browser.contents
+  ME GROK STILL SEE BEAR NAMED Bjorn!
+
+"""
+import grok
+import martian
+import os.path
+
+class PercentTemplate(object):
+    """A simple template class that does Python %-substitution."""
+    def __init__(self, text):
+        self.text = text
+
+    def render(self, **namespace):
+        return self.text % namespace
+
+class PercentPageTemplate(grok.components.GrokPageTemplate):
+    """Glue class suggested by doc/minitutorials/template-languages.txt."""
+    def __init__(self, html):
+        self._template = PercentTemplate(html)
+        self.__grok_module__ = martian.util.caller_module()
+
+    def _initFactory(self, factory):
+        pass
+    
+    def namespace(self, view):
+        namespace = {}
+        namespace['request'] = view.request
+        namespace['view'] = view
+        namespace['context'] = view.context
+        namespace['static'] = view.static
+        return namespace
+    
+    def render(self, view):
+        namespace = self.namespace(view)
+        namespace.update(view.namespace())        
+        return self._template.render(**namespace)
+
+class PercentPageTemplateFile(grok.components.GrokPageTemplate):
+    """Glue class suggested by doc/minitutorials/template-languages.txt."""
+    def __init__(self, filename, _prefix=None):
+        file = open(os.path.join(_prefix, filename))
+        self._template = PercentPageTemplate(file.read())
+        self.__grok_module__ = martian.util.caller_module()
+
+    def render(self, view):
+        return self._template.render(view)
+
+class Bear(grok.Model):
+    def __init__(self, name):
+        self.name = name
+
+class Index(grok.View):
+    def namespace(self):
+        return { 'bear_name': self.context.name }
+
+index = PercentPageTemplateFile('language_file.txt',
+                                os.path.dirname(__file__))
