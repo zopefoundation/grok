72e52097659f295fa1600e463f37dc74cca20be1
Martijn Faassen <faassen@startifact.com>
2007-11-20 12:43:16 +0000

    This needs a newer martian.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     include_package_data = True,
     zip_safe=False,
     install_requires=['setuptools',
-                      'martian >= 0.9.1',
+                      'martian >= 0.9.2',
                       'simplejson',
                       'pytz',
                       'ZODB3',
