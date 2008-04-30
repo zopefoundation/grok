f3bde2dde03dccb005625da5e3680c58ffcc4976
Uli Fouquet <uli@gnufix.de>
2008-04-30 16:06:20 +0000

    Create a proper setup.py for the subpackage.

--- grokdocs/setup.py
+++ grokdocs/setup.py
@@ -2,6 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='grokdocs',
+    version='0.1',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://svn.zope.org/grok/trunk',
+    description="""\
+Grokdocs: Build the Grok documentation in different formats!
+""",
     install_requires=['docutils==0.4',
                       'zope.pagetemplate',
                       'zope.app.renderer',
@@ -9,12 +16,12 @@ setup(
                       'Pygments',
                       'ulif.rest',
                       ],
-    package_dir = {'': 'build'},
-    py_modules = ['grok2html', 'grokdocs'],
+    package_dir = {'': 'src'},
+    packages=find_packages('src'),
     entry_points="""
     [console_scripts]
-    grokdocs2html = grokdocs:grokdocs
-    grokdocs2latex = grokdocs:grokdocs_latex
-    quickstart = grokdocs:sphinxquickstart
+    grokdocs2html = grokdocs.grokdocs:render
+    grokdocs2latex = grokdocs.grokdocs:render_latex
     """
     )
+
