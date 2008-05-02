80a0ece5764a6222ac2cfe7c44078f4d86f22d6a
Philipp von Weitershausen <philipp@weitershausen.de>
2008-05-02 22:26:25 +0000

    Bring back a utility function from grokcore.component that isn't used there at all.

---
+++ src/grok/tests/util/public_methods_from_class.py
@@ -0,0 +1,27 @@
+"""
+  >>> methods = util.public_methods_from_class(A)
+  >>> sorted([m.__name__ for m in methods])
+  ['should_also_be_public', 'should_be_public']
+
+"""
+import grokcore.component.util as util
+
+class A(object):
+
+    def __init__(self):
+        pass # this method is ignored
+
+    def __call__(self):
+        pass # this method is ignored
+
+    def __double_underscored(self):
+        pass # this method is ignored
+
+    def _single_underscored(self):
+        pass # this method is ignored
+
+    def should_be_public(self):
+        pass # this method is found
+
+    def should_also_be_public(self):
+        pass # this method is found
--- src/grok/util.py
+++ src/grok/util.py
@@ -32,10 +32,13 @@ from martian.util import class_annotation
 # grokkers that use these utility functions)
 from grokcore.component.util import check_adapts
 from grokcore.component.util import determine_class_directive
-from grokcore.component.util import public_methods_from_class
 from grokcore.component.util import sort_components
 from grokcore.component.util import determine_module_component
 from grokcore.component.util import determine_class_component
+
+def public_methods_from_class(factory):
+    return [m for m in methods_from_class(factory) if \
+            not m.__name__.startswith('_')]
 
 def make_checker(factory, view_factory, permission, method_names=None):
     """Make a checker for a view_factory associated with factory.
