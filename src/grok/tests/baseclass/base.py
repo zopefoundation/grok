c9840fa4d686978edb7a7411dfdbd91f4247e3fa
Philipp von Weitershausen <philipp@weitershausen.de>
2008-05-03 13:51:07 +0000

    Remove no longer applicable tests (functionality was removed from Martian, but tests in Grok weren't updated yet)

--- src/grok/tests/baseclass/base.py
+++ src/grok/tests/baseclass/base.py
@@ -1,9 +1,6 @@
 """
-Base classes shouldn't be grokked.
-
-One way to indicate that something is a base class is by postfixing the
-classname with 'Base'. Another way is to use the 'grok.baseclass' directive
-on the class itself.
+Base classes shouldn't be grokked. The way is to use the
+'grok.baseclass' directive on the class itself.
 
   >>> grok.testing.grok(__name__)
 
@@ -11,13 +8,6 @@ on the class itself.
   >>> request = TestRequest()
   >>> from zope import component
   >>> model = ModelBase()
-  >>> view = component.getMultiAdapter((model, request), name='viewbase')
-  Traceback (most recent call last):
-    ...
-  ComponentLookupError: ((<grok.tests.baseclass.base.ModelBase object at 0x...>,
-  <zope.publisher.browser.TestRequest instance ...>),
-  <InterfaceClass zope.interface.Interface>,
-  'viewbase')
 
   >>> view = component.getMultiAdapter((model, request), name='anotherview')
   Traceback (most recent call last):
@@ -32,10 +22,6 @@ import grok
 
 class ModelBase(grok.Model):
     pass
-
-class ViewBase(grok.View):
-    def render(self):
-        return "hello world"
 
 class AnotherView(grok.View):
     grok.baseclass()
