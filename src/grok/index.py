5b2da46bbe8fe8ea4d27f7a84a1f46d3f0cda52f
Philipp von Weitershausen <philipp@weitershausen.de>
2008-07-28 23:04:05 +0000

    Add license header

--- src/grok/index.py
+++ src/grok/index.py
@@ -1,3 +1,18 @@
+#############################################################################
+#
+# Copyright (c) 2007-2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Grok index definitions
+"""
 import sys
 
 from zope.interface import implements
