d1b0dd1a22a1694b2e056fbfb31d4f1d18ea56b1
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2008-09-29 17:18:44 +0000

    bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (
 
 setup(
     name='grok',
-    version='0.14dev',
+    version='0.15dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
