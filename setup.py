0ed821e723cd9d82e8a4ffc27b4f03b4c4c8958e
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2008-12-12 15:43:20 +0000

    remove dev marker

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (
 
 setup(
     name='grok',
-    version='0.14.1dev',
+    version='0.14.1',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
