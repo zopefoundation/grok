c8ec6581a2f686750857d9d5a7c353326557aeaf
Christian Klinger <cklinger@novareto.de>
2010-11-04 15:26:25 +0000

    group based ftests

---
+++ src/grok/ftests/security/groups.py
@@ -0,0 +1,69 @@
+"""
+Default Group Behaviour
+-----------------------
+
+Grok provides a default security policy.
+Here we proofe that the basic stuff will work
+as expected.
+
+Note we defined the user and the permission in the ftesting.zcml
+
+  >>> from zope.app.wsgi.testlayer import Browser
+  >>> browser = Browser()
+
+If we try to acces a public site without authentication
+we will get the following goups 'zope.Anybody' and 'zope
+Everybody'
+
+  >>> browser.open("http://localhost/@@publicview")
+  >>> 'zope.Anybody' in browser.contents
+  True
+  >>> 'zope.Everybody' in browser.contents
+  True
+
+If we try to acces a protect view by an anonyoums user
+we will get an Unauthorized Message.
+
+  >>> browser.open("http://localhost/@@protectedview")
+  Traceback (most recent call last):
+  ...
+  HTTPError: HTTP Error 401: Unauthorized
+
+If access the view with an authenticated request we should
+get the groups zope.Authenticated.
+
+  >>> browser.addHeader('Authorization', 'Basic foo:secret')
+  >>> browser.open("http://localhost/@@publicview")
+  >>> 'zope.Authenticated' in browser.contents
+  True
+
+And of course you can access the protected view.
+
+  >>> browser.open("http://localhost/@@protectedview")
+  >>> 'zope.Authenticated' in browser.contents
+  True
+"""
+
+import grok
+import zope.interface
+from zope.component import queryUtility
+from zope.pluggableauth.interfaces import IPrincipalCreated
+from zope.security.interfaces import IGroup, IGroupAwarePrincipal
+from zope.authentication.interfaces import IAuthenticatedGroup, IEveryoneGroup
+
+
+class PublicView(grok.View):
+
+    grok.context(zope.interface.Interface)
+    grok.require('zope.Public')
+
+    def render(self):
+        return ', '.join(self.request.principal.groups)
+
+
+class ProtectedView(grok.View):
+    grok.context(zope.interface.Interface)
+    grok.require('grok.test')
+
+    def render(self):
+        return ', '.join(self.request.principal.groups)
