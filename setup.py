b0385cf8c2079ca5b2b7795210c5bc031f67989c
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2011-07-14 13:53:17 +0000

    Back to development: 1.9

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = [
 
 setup(
     name='grok',
-    version='1.8',
+    version='1.9dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
