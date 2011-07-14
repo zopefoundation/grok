434cb6fa3f017bea55ef4b400c54db27568c494f
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2011-07-14 13:51:45 +0000

    Preparing release 1.8

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = [
 
 setup(
     name='grok',
-    version='1.8dev',
+    version='1.8',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
