cd313718901f91a6340466a003ba9803f60a3d9a
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2012-04-30 13:40:00 +0000

    fix test now that the permissions() directive is in grokcore.security

--- src/grok/tests/security/not_a_permission_class.py
+++ src/grok/tests/security/not_a_permission_class.py
@@ -6,6 +6,6 @@ The permissions() directive only accepts permission ids or permission classes:
   Traceback (most recent call last):
   ...
   GrokImportError: You can only pass unicode values, ASCII values, or
-  subclasses of grok.Permission to the 'permissions' directive.
+  subclasses of grokcore.security.Permission to the 'permissions' directive.
 
 """
