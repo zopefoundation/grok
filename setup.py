400ed69f3e0ac6c82ff16e8325ac51d38e6fde39
Sylvain Viollon <thefunny@gmail.com>
2015-06-04 10:36:31 +0200

    Back to development: 1.14

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = [
 
 setup(
     name='grok',
-    version='1.13',
+    version='1.14.dev0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
