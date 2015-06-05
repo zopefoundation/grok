c93c2bc5e19398a5ce6c00c0530cba32a05bb1e5
Sylvain Viollon <thefunny@gmail.com>
2015-06-05 11:57:33 +0200

    Fix DatabaseCreatedEvent

--- src/grok/generations.py
+++ src/grok/generations.py
@@ -4,16 +4,20 @@ import grok.events
 import zope.event
 
 from zope.generations.interfaces import IInstallableSchemaManager
+from zope.generations.utility import getRootFolder
 
 
-class GrokDatabase(grokcore.component.GlobalUtility):
-    grokcore.component.name('grok.database')
+class GrokDatabaseCreated(grokcore.component.GlobalUtility):
+    # The name starts with a null byte so that then they are sorted and
+    # executed by zope.generation it ends up to be first.
+    grokcore.component.name('\x00grok database created')
     grokcore.component.implements(IInstallableSchemaManager)
 
     minimum_generation = generation = 1
 
     def install(self, context):
-        zope.event.notify(grok.events.DatabaseCreatedEvent(context))
+        root = getRootFolder(context)
+        zope.event.notify(grok.events.DatabaseCreatedEvent(root))
 
     def evolve(self, context, generation):
         pass
