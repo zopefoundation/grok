66a4f80a260b31b8ef628708b8c16651566603d1
Michael Howitz <mh@gocept.com>
2023-09-22 08:27:53 +0200

    Add missing python_requires.

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=3.7',
     install_requires=[
         'grokcore.annotation >= 1.6',
         'grokcore.catalog >= 2.1',
